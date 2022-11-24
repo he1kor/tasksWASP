0e98263ac5d4e04d7cba9bf349dbc0ec40619f11
Helkor <helkor.rightway@gmail.com>
2022-11-25 02:39:57 +0300

    2.1 redone with binary operations

--- Tasks/2/1/Program.cs
+++ Tasks/2/1/Program.cs
@@ -1,4 +1,5 @@
-﻿using Tasks.Util;
+﻿using System.Text;
+using Tasks.Util;
 
 namespace Tasks._2._1;
 
@@ -6,6 +7,42 @@ public class Program : BaseProgram
 {
     public Program()
     {
-        Console.WriteLine(Convert.ToString(Input.ReadNotNegativeInt("Input integer number you want to read as binary:"),2));
+        uint sourceValue = Input.ReadNotNegativeInt("Input integer number you want to read as binary:");
+        Console.WriteLine(IntToBinaryString(sourceValue));
+    }
+
+    private string IntToBinaryString(uint sourceInt)
+    {
+        StringBuilder binaryString = new StringBuilder("");
+        for (int i = 0; i < sizeof(int) * 8; i++)
+        {
+            binaryString.Insert(0,BoolToString(CheckBit(sourceInt,i)));
+        }
+
+        return binaryString.ToString();
+    }
+
+    private uint CreateTrueBitMask(int bitIndex)
+    {
+        TryThrowBitOutOfInt(bitIndex);
+        return (uint)1 << bitIndex;
+    }
+
+    private bool CheckBit(uint value, int bitIndex)
+    {
+        return (value | CreateTrueBitMask(bitIndex)) == value;
+    }
+
+    private void TryThrowBitOutOfInt(int bitIndex)
+    {
+        if (bitIndex < 0)
+            throw new ArgumentOutOfRangeException(bitIndex + " is less than 0");
+        if (bitIndex >= sizeof(int) * 8)
+            throw new ArgumentOutOfRangeException(bitIndex + " is more than " + (sizeof(int)-1));
+    }
+
+    private string BoolToString(bool boolValue)
+    {
+        return boolValue ? "1" : "0";
     }
 }
