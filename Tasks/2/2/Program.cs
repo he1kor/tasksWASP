69a59ebbf64444e4286fea33d9fe35d15baacc1b
Helkor <helkor.rightway@gmail.com>
2022-11-25 02:43:13 +0300

    2.2 redone with binary operations

--- Tasks/2/2/Program.cs
+++ Tasks/2/2/Program.cs
@@ -1,4 +1,5 @@
-﻿using Tasks.Util;
+﻿using System.Text;
+using Tasks.Util;
 
 namespace Tasks._2._2;
 
@@ -10,20 +11,47 @@ public class Program : BaseProgram
         uint secondInt = Input.ReadNotNegativeInt("Input second value:");
         uint sum = firstInt + secondInt;
 
-        string firstBinary = Convert.ToString(firstInt,2);
-        string secondBinary = Convert.ToString(secondInt,2);
-        string sumBinary = Convert.ToString(sum, 2);
-        
-        int firstLength = firstBinary.Length;
-        int secondLength = secondBinary.Length;
-        int sumLength = sumBinary.Length;
-
-        firstBinary = new String('0', sumLength - firstLength) + firstBinary;
-        secondBinary = new String('0', sumLength - secondLength) + secondBinary;
+        string firstBinary = IntToBinaryString(firstInt);
+        string secondBinary = IntToBinaryString(secondInt);
+        string sumBinary = IntToBinaryString(sum);
         
         Console.WriteLine(firstBinary);
         Console.WriteLine(secondBinary);
-        Console.WriteLine(new string('-',sumLength));
+        Console.WriteLine(new string('-',sizeof(int)*8));
         Console.WriteLine(sumBinary);
+    }
+    private string IntToBinaryString(uint sourceInt)
+    {
+        StringBuilder binaryString = new StringBuilder("");
+        for (int i = 0; i < sizeof(int) * 8; i++)
+        {
+            binaryString.Insert(0,BoolToString(CheckBit(sourceInt,i)));
+        }
+
+        return binaryString.ToString();
+    }
+
+    private uint CreateTrueBitMask(int bitIndex)
+    {
+        TryThrowBitOutOfInt(bitIndex);
+        return (uint)1 << bitIndex;
+    }
+
+    private bool CheckBit(uint value, int bitIndex)
+    {
+        return (value | CreateTrueBitMask(bitIndex)) == value;
+    }
+
+    private void TryThrowBitOutOfInt(int bitIndex)
+    {
+        if (bitIndex < 0)
+            throw new ArgumentOutOfRangeException(bitIndex + " is less than 0");
+        if (bitIndex >= sizeof(int) * 8)
+            throw new ArgumentOutOfRangeException(bitIndex + " is more than " + (sizeof(int)-1));
+    }
+
+    private string BoolToString(bool boolValue)
+    {
+        return boolValue ? "1" : "0";
     }
 }
